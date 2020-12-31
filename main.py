1cd94c2867f5083ea203cf1562c302e503c1e926
gmarzioz <gmarzioz@gmail.com>
2020-12-31 16:25:11 +0100

    Updates

--- main.py
+++ main.py
@@ -4,36 +4,25 @@ import os
 
 from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtCore import QFile
-#from PyQt5.QtUiTools import QUiLoader
-from PyQt5 import uic #loadUi
+from PyQt5 import uic  # loadUi
+
 
 class test(QWidget):
     def __init__(self):
         super(test, self).__init__()
-        #self.load_ui()
-        self.atStart()
+        self.load_ui()
 
     def load_ui(self):
-        #loader = QUiLoader()
-        loader = loadUi()
         path = os.path.join(os.path.dirname(__file__), "form.ui")
         ui_file = QFile(path)
         ui_file.open(QFile.ReadOnly)
-        loader.load(ui_file, self)
+        uic.loadUi(ui_file, self)
         ui_file.close()
 
-    def hello(self):
-        print("Definition Hello")
-
-    def atStart(self):
-        print("At start: Called")
 
 if __name__ == "__main__":
-    #print("At first if: called")
+    print("Application start..")
     app = QApplication([])
     widget = test()
-    #widget.show()
-    widget.hello()
-    #widget.atStart()
+    widget.show()
     sys.exit(app.exec_())
-
